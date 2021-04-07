f16a8d6b7b09587179382fdd76c34dfc7ecc63e5
chenxiong <chenx@amigosoft.cn>
2021-04-07 14:56:33 +0800

    图片列表去除多余代码

--- web/src/views/images/index.vue
+++ web/src/views/images/index.vue
@@ -1,51 +1,8 @@
 <template>
   <div class="app-container">
     <div class="search-container">
-      <el-dropdown trigger="click">
-        <el-button type="primary" size="medium" :loading="batchOperateLoading">
-          {{ $t('userManage.userList.batchOperate') }}<i class="el-icon-arrow-down el-icon--right"></i>
-        </el-button>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item @click.native="deleteBatch">{{ $t('userManage.userList.deleteBatch') }}</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <el-input
-        v-model="searchForm.searchUsername"
-        :placeholder="$t('userManage.userList.searchUsername')"
-        clearable
-        size="medium"
-        type="text"
-        style="width: 15%;margin-left: 5px;"
-        @keyup.native="enterSearch(searchForm.searchUsername,$event)"
-        @clear="fetchData(currentPage,pageSize,searchForm)"
-      />
-      <el-input
-        v-model="searchForm.searchEmail"
-        :placeholder="$t('userManage.userList.searchEmail')"
-        clearable
-        size="medium"
-        type="text"
-        style="width: 15%;margin-left: 5px;"
-        @keyup.native="enterSearch(searchForm.searchEmail,$event)"
-        @clear="fetchData(currentPage,pageSize,searchForm)"
-      />
-      <el-input
-        v-model="searchForm.searchCommentCount"
-        :placeholder="$t('userManage.userList.searchCommentCount')"
-        clearable
-        size="medium"
-        type="number"
-        style="width: 18%;margin-left: 5px;"
-        @keyup.native="enterSearch(searchForm.searchCommentCount,$event)"
-        @clear="fetchData(currentPage,pageSize,searchForm)"
-      />
-      <el-button
-        type="primary"
-        size="small"
-        icon="el-icon-search"
-        style="margin-left: 5px;"
-        @click="searchComment"
-      />
+      <el-button type="primary" size="small" icon="el-icon-upload" />
+      <el-button type="primary" size="small" icon="el-icon-sort" />
     </div>
     <el-table
       v-loading="listLoading"
@@ -57,42 +14,45 @@
       style="width: 100%"
       @selection-change="handleSelectionChange"
     >
+      <el-table-column align="center" :label="$t('imagesManage.imageList.table.number.columnName')" width="50">
+        <template slot-scope="scope">
+          {{ scope.$index + 1 }}
+        </template>
+      </el-table-column>
       <el-table-column
-        type="selection"
-        width="40"
-      />
-      <el-table-column align="center" :label="$t('userManage.userList.table.number.columnName')" width="50">
-        <template slot-scope="scope">
-          {{ scope.$index+1 }}
-        </template>
-      </el-table-column>
-      <el-table-column align="left" :label="$t('userManage.userList.table.username.columnName')">
-        <template slot-scope="scope">
-          <router-link
-            tag="a"
-            :to="{ path: '/comment/list', query: { searchUsername: scope.row.username, immediateSearch:true }}"
-          >
-            <a>
-              {{ scope.row.username }}
-            </a>
-          </router-link>
-        </template>
-      </el-table-column>
-      <el-table-column align="left" :label="$t('imagesManage.imageList.table.preview.columnName')">
+        align="center"
+        width="150"
+        :label="$t('imagesManage.imageList.table.preview.columnName')"
+      >
         <template slot-scope="scope">
           <img class="preview" :src="scope.row.url" alt="">
         </template>
       </el-table-column>
-      <el-table-column align="left" :label="$t('imagesManage.imageList.table.filename.columnName')">
+      <el-table-column
+        align="left"
+        prop="fileName"
+        :label="$t('imagesManage.imageList.table.filename.columnName')"
+        :show-overflow-tooltip="true"
+      />
+      <el-table-column
+        align="left"
+        :label="$t('imagesManage.imageList.table.url.columnName')"
+      >
         <template slot-scope="scope">
-          <a v-if="scope.row.url" :href="scope.row.url" target="_blank">{{ scope.row.url }}</a>
-          <span v-else>{{ scope.row.website }}</span>
+          <template v-if="scope.row.url">
+            <a :href="scope.row.url" target="_blank" style="margin-right: 5px">{{ scope.row.url }}</a>
+            <span class="copy-icon" @click="copyUrl(scope.row.url)"><i class="el-icon-document-copy"></i></span>
+          </template>
         </template>
       </el-table-column>
-      <el-table-column align="center" :label="$t('userManage.userList.table.operate.columnName')" :width="$t('userManage.userList.table.operate.width')">
-        <template slot-scope="scope">
-          <el-button type="danger" :loading="operateLoading" @click="deleteUser(scope.row.id)">{{ $t('userManage.userList.table.operate.deleteUser') }}</el-button>
-        </template>
+      <el-table-column
+        align="center"
+        width="150"
+        :label="$t('imagesManage.imageList.table.operate.columnName')"
+      >
+        <!--<template slot-scope="scope">-->
+        <!--<el-button type="danger" :loading="operateLoading" @click="deleteImage()">{{ $t('imagesManage.imageList.delete') }}</el-button>-->
+        <!--</template>-->
       </el-table-column>
     </el-table>
     <el-pagination
@@ -111,6 +71,7 @@
 
 <script>
 import request from '@/utils/request'
+
 export default {
   name: 'List',
   data() {
@@ -138,7 +99,7 @@ export default {
     fetchData(pageNum, pageSize, searchParams) {
       this.listLoading = true
       request({
-        url: '/images/list',
+        url: '/file/images/list',
         method: 'post',
         params: {
           pageNum: pageNum,
@@ -151,15 +112,15 @@ export default {
         this.listLoading = false
       })
     },
-    deleteUser(id) {
-      this.$confirm(this.$t('userManage.message.deleteConfirmMessage'), this.$t('userManage.message.deleteConfirmMessageTitle'), {
-        confirmButtonText: this.$t('userManage.message.confirm'),
-        cancelButtonText: this.$t('userManage.message.cancel'),
+    deleteImage(id) {
+      this.$confirm(this.$t('imagesManage.message.deleteConfirmMessage'), this.$t('imagesManage.message.deleteConfirmMessageTitle'), {
+        confirmButtonText: this.$t('imagesManage.message.confirm'),
+        cancelButtonText: this.$t('imagesManage.message.cancel'),
         type: 'warning'
       }).then(() => {
         this.operateLoading = true
         request({
-          url: '/user/delete',
+          url: '/file/delete',
           method: 'post',
           params: {
             id: id
@@ -176,40 +137,19 @@ export default {
         })
       })
     },
-    deleteBatch() {
-      if (this.multipleSelection.length === 0) {
-        this.$message({
-          message: this.$t('userManage.message.chooseDataDelete'),
-          type: 'error',
-          duration: 3 * 1000,
-          showClose: true
-        })
-        return
-      }
-      this.$confirm(this.$t('userManage.message.deleteConfirmMessage'), this.$t('userManage.message.deleteConfirmMessageTitle'), {
-        confirmButtonText: this.$t('userManage.message.confirm'),
-        cancelButtonText: this.$t('userManage.message.cancel'),
-        type: 'warning'
-      }).then(_ => {
-        this.batchOperateLoading = true
-        request({
-          url: '/user/deleteBatch',
-          method: 'post',
-          data: {
-            id: this.multipleSelection.map(a => a.id)
-          }
-        }).then(response => {
-          this.batchOperateLoading = false
-          this.$message({
-            message: response.message,
-            type: 'success',
-            duration: 3 * 1000,
-            showClose: true
-          })
-          this.fetchData(this.currentPage, this.pageSize)
-        }).catch(() => {
-          this.batchOperateLoading = false
-        })
+    copyUrl(data) {
+      const url = data
+      const oInput = document.createElement('input')
+      oInput.value = url
+      document.body.appendChild(oInput)
+      oInput.select() // 选择对象;
+
+      document.execCommand('Copy') // 执行浏览器复制命令
+      this.$message({
+        message: this.$t('imagesManage.message.copySuccess'),
+        type: 'success',
+        duration: 3 * 1000,
+        showClose: true
       })
     },
     handleSizeChange(val) {
@@ -222,25 +162,24 @@ export default {
     },
     handleSelectionChange(val) {
       this.multipleSelection = val
-    },
-    searchComment() {
-      this.fetchData(this.currentPage, this.pageSize, this.searchForm)
-    },
-    enterSearch(message, event) {
-      // 捕获enter事件
-      if (event.keyCode === 13) {
-        this.fetchData(this.currentPage, this.pageSize, this.searchForm)
-      }
     }
   }
 }
 </script>
 
 <style scoped>
-a:hover{
+a:hover {
   color: #409EFF;
 }
-.preview{
+
+.preview {
   width: 120px;
+}
+
+.copy-icon{
+  cursor: pointer;
+}
+.copy-icon :hover{
+  color: #409EFF;
 }
 </style>
